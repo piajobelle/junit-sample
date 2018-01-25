955c8cc97120a47a10c819122913ade27fca5881
Fidel, Jona Micah V <jona.micah.v.fidel@accenture.com>
2018-01-25 11:37:26 +0800

    try

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,24 +1,15 @@
-
-
+import org.junit.Test;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+public class MyUnitTest {
 
-public class SampleTest {
-	
-	@Test
-	public void simpleTestPassed() {
-		int a = 1;
-		int b = 2;
-		assertTrue(a + b == 54);
-	}
-	
- 	@Test
- 	public void simpleTestFailed() {
- 		int a = 1;
- 		int b = 2;
- 		assertFalse(a + b == 5);
- 		}
- 		
- 		
- 	}
+    @Test
+    public void testConcatenate() {
+        MyUnit myUnit = new MyUnit();
+
+        String result = myUnit.concatenate("one", "two");
+
+        assertEquals("onetwo", result);
+
+    }
+}
